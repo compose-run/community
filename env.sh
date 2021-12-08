7e0f8d98283134ed3f409c6c4c3555871da2ac33
Steve Krouse <steveykrouse@gmail.com>
2021-12-08 12:18:03 -0500

    send envs to .env.development.local

--- env.sh
+++ env.sh
@@ -1,4 +1,6 @@
-REACT_APP_VERCEL_GIT_REPO_OWNER=compose-run
-REACT_APP_VERCEL_GIT_REPO_SLUG=community
-REACT_APP_VERCEL_GIT_COMMIT_REF=$(git symbolic-ref --short HEAD)
-REACT_APP_VERCEL_GIT_COMMIT_SHA=$(git log --pretty=format:'%H' -n 1)
+{
+  echo REACT_APP_VERCEL_GIT_REPO_OWNER=compose-run
+  echo REACT_APP_VERCEL_GIT_REPO_SLUG=community 
+  echo REACT_APP_VERCEL_GIT_COMMIT_REF=$(git symbolic-ref --short HEAD)
+  echo REACT_APP_VERCEL_GIT_COMMIT_SHA=$(git log --pretty=format:'%H' -n 1)
+} > .env.development.local
