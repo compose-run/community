98d680543b01755ae69a37f973c1bcb15e45e1b5
Steve Krouse <steveykrouse@gmail.com>
2021-12-08 11:46:23 -0500

    fix vercel envs

--- src/state/appName.ts
+++ src/state/appName.ts
@@ -1,2 +1,2 @@
-const appName = `${process.env.REACT_APP_VERCEL_GIT_REPO_OWNER}/${process.env.REACT_APP_VERCEL_GIT_REPO_ID}/${process.env.REACT_APP_VERCEL_GIT_COMMIT_REF}`;
+const appName = `@${process.env.REACT_APP_VERCEL_GIT_REPO_OWNER}/${process.env.NEXT_PUBLIC_VERCEL_GIT_REPO_SLUG}/${process.env.REACT_APP_VERCEL_GIT_COMMIT_REF}`;
 export default appName;
