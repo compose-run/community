589194a0afe5a4186bbfde37dcf6fcdad80dd886
Adriaan Leijnse <parenthetical@users.noreply.github.com>
2021-12-08 16:50:30 +0000

    User names cloud reducer.

--- src/state/users.ts
+++ src/state/users.ts
@@ -1,19 +1,26 @@
-// import appName from "./appName";
-// import { useCloudReducer } from "../compose-client-dist/module";
+import { useCloudReducer, User } from "@compose-run/client";
+import appName from "./appName";
 
-// TODO - this should be auto imported from compose-client-dist
-export interface User {
-  id: number;
-  email: string;
-}
+type UserId = number;
 
-// export const useUsers = () =>
-//   useCloudReducer({
-//     name: `${appName}/users`,
-//     initialState: [
-//       {
-//         id: "steve",
-//       },
-//     ],
-//     reducer: (users: User[]) => users,
-//   });
+type UsersDB = { [userId: UserId]: string };
+
+type UserAction = string;
+type UserActionError = string;
+
+// interface SetUserName {
+//   type: "SetUserName";
+//   username: string;
+// }
+
+const usersName = `${appName}/user-settings`;
+
+export const useUsers = () =>
+  useCloudReducer<UsersDB, UserAction, UserActionError>({
+    name: usersName,
+    initialState: {},
+    reducer: (users, action, { resolve, userId }): UsersDB => {
+      users[userId] = action;
+      return users;
+    },
+  }) as [UsersDB, (action: UserAction) => Promise<UserActionError>];
