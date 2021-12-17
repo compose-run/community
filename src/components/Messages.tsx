571d35aab1af1b5b68e4d867b1c8649c00ffc278
Steve Krouse <steveykrouse@gmail.com>
2021-12-17 12:03:38 -0500

    fix build error

--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -2,14 +2,8 @@ import { Channel } from "../App";
 import { useMessages, MessageType, MessageId } from "../state/messages";
 import Message from "./Message";
 
-export default function Messages({
-  channel,
-  replyTo,
-}: {
-  channel: Channel;
-  replyTo?: MessageId;
-}) {
-  const [messages, dispatch] = useMessages();
+export default function Messages({ channel }: { channel: Channel }) {
+  const [messages] = useMessages();
 
   // flex, column-reverse, and extra div trick to make the messages scroll to the bottom
   // https://stackoverflow.com/questions/18614301/keep-overflow-div-scrolled-to-bottom-unless-user-scrolls-up/21067431#comment79201655_44051405
