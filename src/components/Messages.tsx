93782d0847044757e854bd8c2ebaa533a2c73cd9
Steve Krouse <steveykrouse@gmail.com>
2021-12-21 16:10:10 -0500

    bump client v0.0.33

--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -14,7 +14,6 @@ export default function Messages({ channel }: { channel: Channel }) {
         flexGrow: 1,
         marginTop: 10,
         marginBottom: 10,
-        overflowY: "scroll",
         display: "flex",
         flexDirection: "column-reverse",
       }}
